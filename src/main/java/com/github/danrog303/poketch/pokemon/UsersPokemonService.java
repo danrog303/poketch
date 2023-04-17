b8e382903c19df73eae86b2f21d3d8551cab9bb9
Daniel Rogowski <daniel.rogowski@onet.pl>
2023-04-17 20:11:59 +0200

    Fix pokemon ownership change not being persistent

--- src/main/java/com/github/danrog303/poketch/pokemon/UsersPokemonService.java
+++ src/main/java/com/github/danrog303/poketch/pokemon/UsersPokemonService.java
@@ -1,6 +1,7 @@
 package com.github.danrog303.poketch.pokemon;
 
 import com.github.danrog303.poketch.user.details.AppUserDetails;
+import com.github.danrog303.poketch.user.details.AppUserDetailsRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ public class UsersPokemonService {
     @Getter @Setter(onMethod_={@Autowired})
     PokemonRepository pokemonRepository;
 
+    @Getter @Setter(onMethod_={@Autowired})
+    AppUserDetailsRepository appUserDetailsRepository;
+
     @Transactional
     public Set<Long> getPokemonNumbersOfUser(AppUserDetails user) {
         return user.getPokemonCollection().stream()
@@ -24,10 +28,12 @@ public class UsersPokemonService {
     @Transactional
     public void removePokemonFromUser(AppUserDetails user, Pokemon pokemon) {
         user.getPokemonCollection().remove(pokemon);
+        appUserDetailsRepository.save(user);
     }
 
     @Transactional
     public void addPokemonToUser(AppUserDetails user, Pokemon pokemon) {
         user.getPokemonCollection().add(pokemon);
+        appUserDetailsRepository.save(user);
     }
 }
